b73b41f8e69e6b4d93188423f161aed8e0f506b3
Norien <bigdrakar@gmail.com>
2017-03-27 13:03:35 -0400

    a lot of changes
    - restart shell command
    - custom icons
    - moved sensors into a package
    - moved switches into a package
    - added new wemo switch connected to dehumidifier and started a script
    to turn on while mold sensor is tripped.

--- shell/update_ha.sh
+++ shell/update_ha.sh
@@ -1,4 +1,13 @@
-su -s /bin/bash homeassistant
+#!/bin/bash
+
+# Become user 'hass'
+sudo su -s /bin/bash homeassistant <<'EOF'
+# Activate the virtualenv
 source /srv/homeassistant/bin/activate
+# Install Home Assistant
 pip3 install --upgrade homeassistant
-systemctl restart home-assistant@homeserver
+exit
+EOF
+
+# Restart Home Assistant
+sudo systemctl restart home-assistant@homeserver
