f3f5ec979b551a4ae142549b6ce0090a127e7226
Norien <bigdrakar@gmail.com>
2017-04-09 16:38:02 -0400

    kodi_control by @dale3h

--- shell/update_ha.sh
+++ shell/update_ha.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-# Become user 'hass'
-sudo su -s /bin/bash homeassistant <<'EOF'
-# Activate the virtualenv
-source /srv/homeassistant/bin/activate
-# Install Home Assistant
-pip3 install --upgrade homeassistant
-exit
-EOF
-
-# Restart Home Assistant
-sudo systemctl restart home-assistant@homeserver
+hassctl update-hass
