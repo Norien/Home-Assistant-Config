2742b863958290d588da227ed921a0877be228e8
Norien <bigdrakar@gmail.com>
2017-05-18 13:02:39 -0400

    had the error wrong way also this commit is for testing the backup system

--- shell/latest_commit.py
+++ shell/latest_commit.py
@@ -1,2 +1,2 @@
 import os.path, time
-print("%s" % time.ctime(os.path.getmtime("/home/homeassistant/.homeassistant/.git/ORIG_HEAD")))
+print("%s" % time.ctime(os.path.getmtime("/home/homeassistant/.homeassistant/.git/index")))
