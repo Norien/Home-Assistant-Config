75480467ebdb1c5754466701387c870648054eb2
Norien <bigdrakar@gmail.com>
2017-06-01 13:12:32 -0400

    don't back up deps folder

--- shell/backup_config.sh
+++ shell/backup_config.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd /mnt/router_usb && zip -r "Home-Assistant-Backup-$(date +"%Y-%m-%d").zip" /home/homeassistant/.homeassistant/ -x "*.log" -x "/*tts/*" -x "*.db" -x "*.db-shm" -x "*.db-wal" -x "*.mp3"
+cd /mnt/router_usb && zip -r "Home-Assistant-Backup-$(date +"%Y-%m-%d").zip" /home/homeassistant/.homeassistant/ -x "*.log" -x "/*tts/*" -x "*.db" -x "*.db-shm" -x "*.db-wal" -x "*.mp3" -x "/*deps/*"
