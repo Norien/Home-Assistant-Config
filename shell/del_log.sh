ca82a3f404a8ea6a0885c0d62b61cff5ff10a3f0
Norien <bigdrakar@gmail.com>
2017-07-19 12:03:05 -0400

    clean up some shell

--- shell/del_log.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rm /home/homeassistant/.homeassistant/home-assistant.log && touch /home/homeassistant/.homeassistant/home-assistant.log && chown -R -v homeassistant:homeassistant /home/homeassistant/.homeassistant/home-assistant.log
